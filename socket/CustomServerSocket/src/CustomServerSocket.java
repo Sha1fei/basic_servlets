c672a5206b85fefe18caafb538744f2109d44e59
Valentin Zadorozhniy <someaderss3000@gmail.com>
2024-11-18 21:14:30 +0700

    add UDP server

--- socket/CustomServerSocket/src/CustomServerSocket.java
+++ socket/CustomServerSocket/src/CustomServerSocket.java
@@ -1,17 +1,22 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.*;
 import java.util.Scanner;
 
 public class CustomServerSocket {
     public static void main(String[] args) {
+        CustomServerSocket.runHttpServer();
+        CustomServerSocket.runUDPServer(); // запускать либо http либо udp
+    }
+
+    public static void runHttpServer(){
         try (var serverSocket = new ServerSocket(7777);
              var socket = serverSocket.accept();
              var outputStream = new DataOutputStream(socket.getOutputStream());
              var inputStream = new DataInputStream(socket.getInputStream());
              var scanner = new Scanner(System.in)
-             ) {
+        ) {
             var request = inputStream.readUTF();
             while (!"exit".equalsIgnoreCase(request)) {
                 System.out.println("Client: " + request);
@@ -19,6 +24,17 @@ public class CustomServerSocket {
                 outputStream.writeUTF(response);
                 request = inputStream.readUTF();
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void runUDPServer(){
+        try (var datagramServer = new DatagramSocket(7777);) {
+            byte[] buffer = new byte[21];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            datagramServer.receive(packet);
+            System.out.println(new String(buffer));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- socket/CustomSocket/src/CustomSocket.java
+++ socket/CustomSocket/src/CustomSocket.java
@@ -1,6 +1,8 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.Socket;
 import java.util.Scanner;
@@ -8,7 +10,7 @@ import java.util.Scanner;
 public class CustomSocket {
     public static void main(String[] args) {
         CustomSocket.runHttpClient();
-//        CustomSocket.runUDPClient();
+        CustomSocket.runUDPClient(); // запускать либо http либо udp
     }
     public static void runHttpClient(){
         try {
@@ -28,22 +30,16 @@ public class CustomSocket {
             throw new RuntimeException(e);
         }
     }
-//    public static void runUDPClient(){
-//        try {
-//            var inetAddress = Inet4Address.getByName("localhost");
-//            try (var  = new Socket(inetAddress, 7777);
-//                 var outputStream = new DataOutputStream(socket.getOutputStream());
-//                 var inputStream = new DataInputStream(socket.getInputStream());
-//                 var scanner = new Scanner(System.in)) {
-//                while (scanner.hasNextLine()) {
-//                    var request = scanner.nextLine();
-//                    outputStream.writeUTF(request);
-//                    var response = inputStream.readUTF();
-//                    System.out.println("Server: " + response);
-//                }
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    public static void runUDPClient(){
+        try {
+            var inetAddress = Inet4Address.getByName("localhost");
+            try (var datagramSocket = new DatagramSocket();) {
+                var bytes = "Hello from UDP client".getBytes();
+                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, inetAddress, 7777);
+                datagramSocket.send(packet);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
